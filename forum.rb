40174a2cd6d8ea298d10ebeea6bdd243d726e744
Max Peterson <petersonmaxr@gmail.com>
2015-06-09 10:22:13 -0400

    changed env to ENV

--- forum.rb
+++ forum.rb
@@ -15,7 +15,7 @@ module Forum
 
 	  configure :production do
 	  	require 'uri'
-			uri = URI.parse env["DATABASE_URL"]
+			uri = URI.parse ENV["DATABASE_URL"]
 			$db = PG connect dbname: uri.path[1..-1], host: uri.host, port: uri.port, user: uri.user, password: uri.password
 	  end
 
